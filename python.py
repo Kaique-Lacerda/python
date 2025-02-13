29189dfdc17fc2183eb466cc3809d4cbcff74cd9
Kaique Gabriel <149600790+Opimalak@users.noreply.github.com>
2025-02-13 18:03:13 +0000

    \

--- python.py
+++ python.py
@@ -6,5 +6,23 @@ numero_secreto = 89
 total_tentativas = 10
 rodada =  1
 
-while (rodada <= total_tentativas);
+while (rodada <= total_tentativas):
+print(f"tentativa {rodada} de {total_tentativas}")
 
+chute_str = input ("digite seu número:")
+print ("você digitou o numero:", chute_str)
+chute = int(chute_str)
+
+acertou = chute == numero_secreto
+maior = chute > numero_secreto
+menor = chute < numero_secreto
+
+if (acertou):
+    ("NICE FILHOTE")
+else:
+    if (maior):
+        ("É MAIOR")
+    elif(menor):
+        ("É MENOR") 
+    rodada = rodada + 1
+    print("fim de jogo")
