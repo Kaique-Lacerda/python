88e1ca536e84b2f59ec9b0afba4375dfc58fbdef
Kaique Gabriel <149600790+Opimalak@users.noreply.github.com>
2025-03-06 18:08:28 +0000

    a

--- Forca.py
+++ Forca.py
@@ -4,24 +4,37 @@ print ("********************Bem vindo ao jogo da forca!!!*********************")
 print ("**********************************************************************")
 print ("")
 
-Ps = "sucuzinho"
+Ps = "sucuzinho".upper()
 
-La = ["_","_"."_","_","_"]
+La = ["_","_","_","_","_"]
 print(La)
 
 enforcou = False
 acertou = False
+erros = 0
 
 while(not enforcou and not acertou):
     
     chute = input("Qual a letra? ")
-    chute = chute.strip()
+    chute = chute.strip().upper()
 
-    index = 0
+    if(chute in Ps):
+        index = 0
 
-    for letra in Ps:
-        if(chute.upper() == letra.upper()):
-            La[index] = letra
-        index = index + 1
-    print("Jogando...")
+        for letra in Ps:
+            if(chute.upper() == letra.upper()):
+                La[index] = letra
+            index = index + 1
+    else:
+        erros = erros + 1
+
+    enforcou = erros == 6
+    acertou = "_" not in La
+    print(La)
+
+if(acertou):
+    print("GGWP")
+else:
+    print(erros)
+    print(La)
 print("Fim de Jogo")
