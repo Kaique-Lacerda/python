b992deac335c90d54b706958ea51e7d291ba1359
Kaique Gabriel <149600790+Kaique-Lacerda@users.noreply.github.com>
2025-09-05 19:33:36 +0000

    a

---
+++ lutaMedieval.py
@@ -0,0 +1,8 @@
+import random 
+print ("Bem-vindo à luta medieval!")
+print ("Você é um cavaleiro, que deve derrotar o mal para salvar o princeso trans femboy therian!!")
+print ("Lute contra o lado negro para salvar o mundo")
+
+vida_jogador = 1000
+vida_inimigo = 200000
+dano base 
